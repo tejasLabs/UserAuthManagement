88a66a96d4a4d1cf6ee82ac2cedc1ef927b0d9e5
Tejas <94tejas@gmail.com>
2026-02-13 17:28:25 +0530

    Added proper management for related classes in User class with proper annotations.

--- src/main/java/com/UserAuthMicroservice/models/User.java
+++ src/main/java/com/UserAuthMicroservice/models/User.java
@@ -3,9 +3,11 @@ package com.UserAuthMicroservice.models;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.ManyToMany;
@@ -27,17 +29,22 @@ public class User extends BaseModel {
     @Column(nullable = false)
     private String username;
 
-
     @Column(nullable = false, unique = true)
     private String email;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    // Setting CascadeType.ALL to ensure that all operations (persist, merge,
+    // remove, refresh, detach) are cascaded to the associated Role entity
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Role> roles = new HashSet<>();
 
     @Column(nullable = false)
     private String password;
 
-    @OneToOne(optional = false)
+    // Set subscription_id as the foreign key to Subscription table
+    // Setting CascadeType.ALL to ensure that all operations (persist, merge,
+    // remove, refresh, detach) are cascaded to the associated Subscription entity
+    @OneToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "subscription_id", referencedColumnName = "id", nullable = false)
     private Subscription subscription;
 
 }
