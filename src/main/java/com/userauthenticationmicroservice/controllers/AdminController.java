42a33ed9b911e9fbac6005999e3162d2f6536164
Tejas <94tejas@gmail.com>
2026-02-25 11:44:40 +0530

    Added functionality for Admin authority users to make changes to roles, subscriptions, users as required.

--- src/main/java/com/userauthenticationmicroservice/controllers/AdminController.java
+++ src/main/java/com/userauthenticationmicroservice/controllers/AdminController.java
@@ -2,11 +2,15 @@ package com.userauthenticationmicroservice.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.userauthenticationmicroservice.dtos.RoleRequestDTO;
 import com.userauthenticationmicroservice.dtos.SortParam;
+import com.userauthenticationmicroservice.dtos.SubscriptionRequestDTO;
+import com.userauthenticationmicroservice.models.SubscriptionType;
 import com.userauthenticationmicroservice.repositories.projections.UserInfo;
 import com.userauthenticationmicroservice.repositories.projections.UserSummary;
 import com.userauthenticationmicroservice.services.AdminService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -15,9 +19,14 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,6 +50,45 @@ public class AdminController {
         return ResponseEntity.status(HttpStatus.OK).body(adminService.getUserInfo(userId));
     }
 
-    
-    
+    @PatchMapping("/users/{userId}/suspend")
+    public ResponseEntity<String> suspendUser(@NonNull @PathVariable UUID userId) {
+        adminService.suspendUser(userId);
+        return ResponseEntity.ok("User " + userId + " suspended successfully.");
+    }
+
+    @PatchMapping("/users/{userId}/restore")
+    public ResponseEntity<String> activateUser(@NonNull @PathVariable UUID userId) {
+        adminService.restoreUser(userId);
+        return ResponseEntity.ok("User " + userId + " activated successfully.");
+    }
+
+    @PatchMapping("/users/{userId}/delete")
+    public ResponseEntity<String> deleteUser(@NonNull @PathVariable UUID userId) {
+        adminService.softDeleteUser(userId);
+        return ResponseEntity.ok("User " + userId + " marked for deletion successfully.");
+    }
+
+    @PatchMapping("/users/{userId}/hardDelete")
+    public ResponseEntity<String> hardDeleteUser(@NonNull @PathVariable UUID userId){
+        adminService.hardDeleteUser(userId);
+        return ResponseEntity.ok("User " + userId + " deleted permanently.");
+    }
+
+    @PatchMapping("/users/updateSubscription")
+    public ResponseEntity<String> updateSubscription(@Valid @RequestBody SubscriptionRequestDTO subscriptionRequestDto){
+        adminService.updateSubscription(subscriptionRequestDto.userId(), subscriptionRequestDto.subscriptionType(), subscriptionRequestDto.endDate());
+        return ResponseEntity.ok("User " + subscriptionRequestDto.userId() + " subscription updated successfully with" + (subscriptionRequestDto.subscriptionType() != null ? " subscription type: " + subscriptionRequestDto.subscriptionType() : "") + (!subscriptionRequestDto.subscriptionType().equals(SubscriptionType.LIFETIME.toString()) ? " end date: " + subscriptionRequestDto.endDate() : " end date: NA"));
+    }
+
+    @PostMapping("/users/add/roles")
+    public ResponseEntity<String> addUserRole(@Valid @RequestBody RoleRequestDTO roleRequestDTO){
+        adminService.addUserRole(roleRequestDTO.userId(), roleRequestDTO.role());
+        return ResponseEntity.status(HttpStatus.CREATED).body("User " + roleRequestDTO.userId() + " has been assigned the role " + roleRequestDTO.role() + " successfully.");
+    }
+
+    @DeleteMapping("/users/remove/roles")
+    public ResponseEntity<String> removeUserRole(@Valid @RequestBody RoleRequestDTO roleRequestDTO){
+        adminService.removeUserRole(roleRequestDTO.userId(), roleRequestDTO.role());
+        return ResponseEntity.status(HttpStatus.OK).body("Role " + roleRequestDTO.role() + " has been removed from user " + roleRequestDTO.userId() + " successfully.");
+    }
 }
--- src/main/java/com/userauthenticationmicroservice/services/AdminService.java
+++ src/main/java/com/userauthenticationmicroservice/services/AdminService.java
@@ -1,28 +1,44 @@
 package com.userauthenticationmicroservice.services;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.userauthenticationmicroservice.dtos.SortParam;
 import com.userauthenticationmicroservice.dtos.SortType;
+import com.userauthenticationmicroservice.exceptions.SubscriptionNotFoundException;
 import com.userauthenticationmicroservice.exceptions.UserNotFoundException;
+import com.userauthenticationmicroservice.exceptions.UserSuspendedException;
+import com.userauthenticationmicroservice.models.Role;
+import com.userauthenticationmicroservice.models.Status;
+import com.userauthenticationmicroservice.models.Subscription;
+import com.userauthenticationmicroservice.models.SubscriptionType;
+import com.userauthenticationmicroservice.models.User;
+import com.userauthenticationmicroservice.repositories.RoleRepository;
 import com.userauthenticationmicroservice.repositories.UserRepository;
 import com.userauthenticationmicroservice.repositories.projections.UserInfo;
 import com.userauthenticationmicroservice.repositories.projections.UserSummary;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     
+    @Transactional(readOnly = true)
     public Page<UserSummary> getAllUsers(int page, int size, List<SortParam> sortParams){
         
         if(sortParams == null || sortParams.isEmpty()){
@@ -45,11 +61,95 @@ public class AdminService {
         return userRepository.findAllProjectedBy(PageRequest.of(page,size,sort), UserSummary.class);
     }
 
+    @Transactional(readOnly = true)
     public UserInfo getUserInfo(UUID userId){
         UserInfo userInfo = userRepository.findAllProjectedById(userId);
         if(userInfo == null){
             throw new UserNotFoundException();
         }
         return userInfo;
+    }
+
+    public void suspendUser(@NonNull UUID userId){
+        User user =userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        if(user.getStatus().equals(Status.DELETED)){
+            throw new UserNotFoundException();
+        }
+        user.setStatus(Status.SUSPENDED);
+        userRepository.save(user);
+    }
+
+    public void softDeleteUser(@NonNull UUID userId){
+        User user =userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        user.setStatus(Status.DELETED);
+        user.getSubscription().setStatus(Status.DELETED);
+        userRepository.save(user);
+    }
+
+    public void restoreUser(@NonNull UUID userId){
+        User user =userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        user.setStatus(Status.ACTIVE);
+        userRepository.save(user);
+    }
+
+    public void hardDeleteUser(@NonNull UUID userId){
+        User user =userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        if(user==null)
+            throw new UserNotFoundException();
+        userRepository.delete(user);
+    }
+
+    public void updateSubscription(@NonNull UUID userId, String subscriptionType, Instant endDate){
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (userOpt.isEmpty() || userOpt.get().getStatus().equals(Status.DELETED)) {
+            throw new UserNotFoundException();
+        }
+        else if(userOpt.get().getStatus().equals(Status.SUSPENDED)){
+            throw new UserSuspendedException();
+        }
+        User user = userOpt.get();
+        Subscription subscription = user.getSubscription();
+        if(subscription == null){
+            throw new SubscriptionNotFoundException();
+        }
+        subscription.setSubscriptionType(SubscriptionType.valueOf(subscriptionType));
+        subscription.setStatus(Status.ACTIVE);
+        if(subscriptionType.equals(SubscriptionType.LIFETIME.toString())){
+            subscription.setEndDate(null);
+        }
+        else{
+            subscription.setEndDate(endDate);
+        }
+        userRepository.save(user);
+    }
+
+    public void addUserRole(@NonNull UUID userId, String newRole) {
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (userOpt.isEmpty() || userOpt.get().getStatus().equals(Status.DELETED)) {
+            throw new UserNotFoundException();
+        }
+        else if(userOpt.get().getStatus().equals(Status.SUSPENDED)){
+            throw new UserSuspendedException();
+        }
+
+        User user = userOpt.get();
+        Role roleToAdd = roleRepository.findByValue(newRole)
+                                            .orElseGet(() -> roleRepository.save(new Role(newRole)));
+        user.getRoles().add(roleToAdd);
+        userRepository.save(user);
+    }
+
+    public void removeUserRole(@NonNull UUID userId, String roleToRemove){
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (userOpt.isEmpty() || userOpt.get().getStatus().equals(Status.DELETED)) {
+            throw new UserNotFoundException();
+        }
+        else if(userOpt.get().getStatus().equals(Status.SUSPENDED)){
+            throw new UserSuspendedException();
+        }
+
+        User user = userOpt.get();
+        user.getRoles().removeIf(role -> Objects.equals(role.getValue(), roleToRemove));
+        userRepository.save(user);
     }
 }
